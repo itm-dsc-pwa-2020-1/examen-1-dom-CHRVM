037122d5f0c024dc92a6aa495a39ca2e53e6245a
CHRVM <61295676+CHRVM@users.noreply.github.com>
2020-02-25 13:37:23 -0600

    Add files via upload

---
+++ RopaDOMExamenu1/js/funciones.js
@@ -0,0 +1,27 @@
+function dark(){
+    document.getElementById('arriba').style.background='#696969';
+    document.getElementById('1enlaces').style.background='#000000';
+    document.getElementById('2enlaces').style.background='#0A004C';
+    document.getElementById('medio').style.background='#000000';
+    document.getElementById('body').style.background='#000000';
+    document.getElementById('abajo').style.background='#0A004C';
+
+    document.getElementById('lista1').style.color='#B10000';
+    document.getElementById('lista2').style.color='#B10000';
+    document.getElementById('lista3').style.color='#B10000';
+    document.getElementById('lista4').style.color='#B10000';
+    document.getElementById('lista5').style.color='#B10000';
+    document.getElementById('lista6').style.color='#B10000';
+
+    document.getElementById('lista7').style.color='#B10000';
+    document.getElementById('lista8').style.color='#B10000';
+    document.getElementById('lista9').style.color='#B10000';
+
+    document.getElementById('lista10').style.color='#B10000';
+
+
+}
+
+function normal(){
+location.reload(true);
+}
